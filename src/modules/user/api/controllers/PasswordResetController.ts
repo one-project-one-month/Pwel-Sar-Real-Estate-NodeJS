e3937d51d9c141fb9a852bed5b1d003e124e5883
nainghtetlinn <naing.work.it@gmail.com>
2025-07-13 13:15:50 +0630

    feat: implement password reset functionality in PasswordResetController

--- src/modules/user/api/controllers/PasswordResetController.ts
+++ src/modules/user/api/controllers/PasswordResetController.ts
@@ -1,10 +1,31 @@
 import { NextFunction, Request, Response } from 'express';
+import { PasswordResetUseCase } from 'modules/user/applications/usecase/passwordResetToken/PasswordResetUseCase';
 import { RequestPasswordResetTokenUseCase } from 'modules/user/applications/usecase/passwordResetToken/RequestPasswordResetTokenUseCase';
 import { catchErrorAsync } from 'utils/error-handling';
 
 import { Container } from '../di/Container';
 
 export class PasswordResetController {
+  async passwordReset(req: Request, res: Response, next: NextFunction) {
+    const { email, newPassword, token } = req.body;
+
+    const passwordResetUseCase = new PasswordResetUseCase(
+      Container.authRepository,
+      Container.passwordResetRepository
+    );
+
+    const [error, result] = await catchErrorAsync(
+      passwordResetUseCase.execute(email, newPassword, token)
+    );
+
+    if (error) {
+      next(error);
+      return;
+    }
+
+    res.status(200).json({ data: result });
+  }
+
   async requestPasswordResetToken(
     req: Request,
     res: Response,
